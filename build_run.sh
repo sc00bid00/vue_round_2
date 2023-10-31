265c49183f90cbc1407a99ca6fe1b3f884b7b853
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-10-31 09:37:58 +0100

    sidebar cart finished

--- build_run.sh
+++ build_run.sh
@@ -3,4 +3,4 @@ if docker ps -aq; then docker rm -f $(docker ps -aq); fi
 if docker image ls -aq; then docker image prune -af; fi
 
 docker build -t vue .
-docker run --name vue -it -p 8080:8080 -v $(pwd)/simple_app:/simple-app/ vue bash
+docker run --name vue -it -p 8080:8080 -v $(pwd)/simple_app:/simple_app/ vue bash
